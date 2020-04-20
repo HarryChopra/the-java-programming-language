07a3d0940c1fecace6302c48ada47874f431cb2b
Harry Chopra <harrychopra@gmail.com>
2020-04-20 19:36:11 +0100

    Done

---
+++ java-1/src/MultiplicationTable.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+
+public class MultiplicationTable {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Give the number: ");
+        int num = Integer.parseInt(reader.next());
+        printTable(num);
+    }
+
+    public static void printTable(int max) {
+        for (int i = 1; i <= max; i++) {
+            printRow(i, (i * max));
+            System.out.println("");
+        }
+    }
+
+    public static void printRow(int coeff, int max) {
+        for (int i = coeff; i <= max; i += coeff) {
+            System.out.print(i + " ");
+        }
+    }
+
+}
